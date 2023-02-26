8136036c6d80a484a8532bfb9ec8c1a0ba20a733
MVDW <maxvdwolf@gmail.com>
2023-02-26 14:11:57 +0100

    add default config file creation

--- bas.py
+++ bas.py
@@ -1,12 +1,20 @@
 import sys, os
-
+import json
 
 bas_version="0.0.1 in-dev"
 bas_lang = "en-US"
+command_type=""
 
 def main(argv):
-	operation=argv[0]
-	match operation:
+	if(len(argv) == 0):
+		print_help()
+		sys.exit(os.EX_OK)
+
+	read_configuration()
+
+	command_type=argv[0]
+
+	match command_type:
 		case "-h" | "--help" | "help" | "h":
 			print_help()
 		case "-v" | "--version" | "version" | "v":
@@ -17,14 +25,38 @@ def main(argv):
 			print("Unknown argument, please do `bas --help` for help")
 	
 def print_help():
-	doc_help_path=resolve_path("lang/" + bas_lang + "/doc_help.txt")
-	doc_help_file=open(doc_help_path, 'r')
+	doc_help_file=open(resolve_path("lang/" + bas_lang + "/doc_help.txt"), 'r')
 	doc_help_content = doc_help_file.read()
 	print(doc_help_content)
 	doc_help_file.close()
 
 def resolve_path(path):
 	return os.path.join(os.path.abspath(os.path.dirname(__file__)), path)
+
+def read_configuration():
+	configuration_path_name = "/etc/bas/"
+	configuration_file_name = "config.json"
+	os.makedirs(configuration_path_name, exist_ok=True)
+
+	if os.path.exists(configuration_path_name + configuration_file_name):
+		# TODO: Read out config file
+		config_file = open(configuration_path_name + configuration_file_name, "r")
+		config_data = json.loads(config_file.read())
+		if config_data["config_version"] != 1:
+			print(f"{color.FAIL}ERROR: Configuration version doesn't match the required version.\nTo fix this you need to manually upgrade your configuration file in /etc/bas/config.json{color.ENDC}")
+			sys.exit(os.EX_CONFIG)
+		config_file.close()
+	else:
+		create_config()
+
+
+def create_config():
+	default_config_file=open(resolve_path("default_config.json"), 'r')
+	default_config_content = default_config_file.read()
+	config_file = open("/etc/bas/config.json", "a")
+	config_file.write(default_config_content)
+	config_file.close()
+	default_config_file.close()
 
 class color:
 	HEADER = '\033[94m'
