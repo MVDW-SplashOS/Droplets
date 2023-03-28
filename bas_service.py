47f67404cd33b817f7487facb5517be8065ad982
MVDW <maxvdwolf@gmail.com>
2023-03-28 15:23:04 +0200

    Add BAS service

---
+++ bas_service.py
@@ -0,0 +1,33 @@
+import re
+import subprocess
+import time
+
+
+
+devices = []
+
+
+
+print("[INFO] Starting BAS Service...")
+
+
+
+
+def check_new_devices():
+
+    device_re = re.compile(b"Bus\s+(?P<bus>\d+)\s+Device\s+(?P<device>\d+).+ID\s(?P<id>\w+:\w+)\s(?P<tag>.+)$", re.I)
+    df = subprocess.check_output("lsusb")
+
+    for i in df.split(b'\n'):
+        if i:
+            info = device_re.match(i)
+            if info:
+                dinfo = info.groupdict()
+                dinfo['device'] = '/dev/bus/usb/%s/%s' % (dinfo.pop('bus'), dinfo.pop('device'))
+                if dinfo not in devices:
+                    devices.append(dinfo)
+                    print("[INFO] New Device: ", dinfo)
+            
+while True:
+    check_new_devices()
+    time.sleep(0.5)
--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 pyinstaller -F --add-data "./include:." bas.py 
+pyinstaller -F --add-data "./include:." bas_service.py 
 echo "--------------"
 echo "output in: $(pwd)/dist" 
