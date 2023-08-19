12adbee193ac75757eebc27abc1b0bcee0c31b7b
MVDW <maxvdwolf@gmail.com>
2023-08-19 13:58:19 +0200

    Fixed regex being vulnerable to polynomial runtime denail of service attack.

--- src/service/device.py
+++ src/service/device.py
@@ -15,7 +15,7 @@ print("[INFO] Starting BAS Service...")
 
 def check_new_devices():
 
-    device_re = re.compile(b"Bus\s+(?P<bus>\d+)\s+Device\s+(?P<device>\d+).+ID\s(?P<id>\w+:\w+)\s(?P<tag>.+)$", re.I)
+    device_re = re.compile(b"Bus\s+(?P<bus>\d++)\s+Device\s+(?P<device>\d++).+?ID\s(?P<id>\w+:\w++)\s(?P<tag>.+)$", re.I | re.DOTALL)
     df = subprocess.check_output("lsusb")
 
     for i in df.split(b'\n'):
